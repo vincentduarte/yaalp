65641da743da6cba931c7e42d92eed2918cda7a8
jamon.ben <jamon.ben@c2180ad6-5146-0410-adca-dfa11db1ecd2>
2008-07-05 20:49:16 +0000

    Added pink noise synthesis.

--- trunk/csaudio/WaveAudio/WaveAudio/Synthesis.cs
+++ trunk/csaudio/WaveAudio/WaveAudio/Synthesis.cs
@@ -72,36 +72,6 @@ namespace CsWaveAudio
     }
 
 
-    public class WhiteNoise : AperiodicSimpleSynthesisBase
-    {
-        private Random rand;
-        public WhiteNoise(double amp) : base(amp) { rand = new Random(); }
-        protected override double WaveformFunction(int i)
-        {
-            return (rand.NextDouble() * 2 - 1.0); //random number between -1 and 1
-        }
-    }
-
-    // like a random walk. Can also be thought of as integral of white noise
-    public class RedNoise : AperiodicSimpleSynthesisBase
-    {
-        private Random rand;
-        private double location, factor;
-        public RedNoise(double amp) : this(amp, 2.0) { }
-        public RedNoise(double amp, double factor)
-            : base(amp)
-        {
-            rand = new Random(); location = 0; this.factor = factor;
-        }
-        protected override double WaveformFunction(int i)
-        {
-            location += (rand.NextDouble() * 2 - 1.0) * factor;
-            if (location > 1) location = 1;
-            else if (location < -1) location = -1;
-            return location;
-        }
-    }
-
     // On a whim I thought, what if instead of a sine wave I used semi-circles!
     // the result doesn't sound too interesting, but I use it for my "electric organ" effect.
     internal class CircleWave : PeriodicSynthesisBase
@@ -118,10 +88,77 @@ namespace CsWaveAudio
         protected override double WaveformFunction(int i)
         {
             if (i % period < halfperiod)
-                return amplitude * Math.Sqrt(1 - Math.Pow(((i % period) / qtrperiod - 1), 2.0));
+                return Math.Sqrt(1 - Math.Pow(((i % period) / qtrperiod - 1), 2.0));
             else
-                return -amplitude * Math.Sqrt(1 - Math.Pow((((i % period) - halfperiod) / qtrperiod - 1), 2.0));
+                return -1.0 * Math.Sqrt(1 - Math.Pow((((i % period) - halfperiod) / qtrperiod - 1), 2.0));
         }
     }
+
+    public class WhiteNoise : AperiodicSimpleSynthesisBase
+    {
+        private Random rand;
+        public WhiteNoise(double amp) : base(amp) { rand = new Random(); }
+        protected override double WaveformFunction(int i)
+        {
+            return (rand.NextDouble() * 2 - 1.0); //random number between -1 and 1
+        }
+    }
+
+    // like a random walk. Can also be thought of as integral of white noise
+    // note: the weights on this may not be correct, at least the spectrum doesn't match with Audacity's brown noise
+    public class RedNoise : AperiodicSimpleSynthesisBase
+    {
+        private Random rand;
+        private double location, factor;
+        // the default "factor" was changed from 2.0 to 0.1 after comparing with Audacity's brown noise.
+        public RedNoise(double amplitude) : this(amplitude, 0.1) { }
+        public RedNoise(double amplitude, double factor)
+            : base(amplitude)
+        {
+            rand = new Random(); location = 0; this.factor = factor;
+        }
+        protected override double WaveformFunction(int i)
+        {
+            location += (rand.NextDouble() * 2 - 1.0) * factor;
+            if (location > 1) location = 1;
+            else if (location < -1) location = -1;
+            return location;
+        }
+    }
+
+
+    public class PinkNoise : AperiodicSimpleSynthesisBase
+    {
+        // http://home.earthlink.net/~ltrammell/tech/pinkalg.htm
+        // spectrum looks pretty good. 
+        // Every sample is multiplied by 30 to increase the amplitude, but this will not affect the spectrum.
+        private Random rand;
+        private readonly double[] av = new double[] { 4.6306e-003,  5.9961e-003,  8.3586e-003};
+        private readonly double[] pv = new double[] { 3.1878e-001, 7.7686e-001, 9.7785e-001 };
+        private double[] randreg;
+        public PinkNoise(double amplitude)
+            : base(amplitude)
+        {
+            rand = new Random();
+            
+            // Initialize the randomized sources state
+            randreg = new double[av.Length];
+            for (int i = 0; i < av.Length; i++)
+                randreg[i] = av[i] * 2 * (rand.NextDouble() - 0.5);
+        }
+        protected override double WaveformFunction(int sampleIndex)
+        {
+            double rv = rand.NextDouble();
+            // Update each generator state per probability schedule
+            for (int i = 0; i < av.Length; i++)
+            {
+                if (rv > pv[i])
+                    randreg[i] = av[i] * 2 * (rand.NextDouble() - 0.5);
+            }
+            // Signal is the sum of the generators
+            return (randreg[0] + randreg[1] + randreg[2]) * 30.0;
+        }
+    }
+
 }
 
