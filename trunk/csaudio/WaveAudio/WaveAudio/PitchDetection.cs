6e6ecfb20741aeecfd8344a28cc703b4d73c4aca
jamon.ben <jamon.ben@c2180ad6-5146-0410-adca-dfa11db1ecd2>
2008-07-06 16:55:49 +0000

    removed method

--- trunk/csaudio/WaveAudio/WaveAudio/PitchDetection.cs
+++ trunk/csaudio/WaveAudio/WaveAudio/PitchDetection.cs
@@ -45,12 +45,14 @@ namespace CsWaveAudio
             return detectPitchCalculation(w, minHz, maxHz, nCandidates, nResolution, algorithm);
         }
 
-
+        // These work by shifting the signal until it seems to correlate with itself.
+        // In other words if the signal looks very similar to (signal shifted 200 samples) than the fundamental period is probably 200 samples
+        // Note that the algorithm only works well when there's only one prominent fundamental.
         private static double[] detectPitchCalculation(WaveAudio w, double minHz, double maxHz, int nCandidates, int nResolution, PitchDetectAlgorithm algorithm)
         {
             // note that higher frequency means lower period
-            int nLowPeriodInSamples = HzToPeriodInSamples(maxHz, w.getSampleRate());
-            int nHiPeriodInSamples = HzToPeriodInSamples(minHz, w.getSampleRate());
+            int nLowPeriodInSamples = hzToPeriodInSamples(maxHz, w.getSampleRate());
+            int nHiPeriodInSamples = hzToPeriodInSamples(minHz, w.getSampleRate());
             if (nHiPeriodInSamples <= nLowPeriodInSamples) throw new Exception("Bad range for pitch detection.");
             if (w.getNumChannels() != 1) throw new Exception("Only mono supported.");
             double[] samples = w.data[0];
@@ -93,7 +95,7 @@ namespace CsWaveAudio
             // convert back to Hz
             double[] res = new double[nCandidates];
             for (int i=0; i<nCandidates;i++)
-                res[i] = PeriodInSamplesToHz(bestIndices[i]+nLowPeriodInSamples, w.getSampleRate());
+                res[i] = periodInSamplesToHz(bestIndices[i]+nLowPeriodInSamples, w.getSampleRate());
             return res;
         }
 
@@ -122,49 +124,18 @@ namespace CsWaveAudio
                 // now blank out that index.
                 inputs[nBestIndex] = double.MinValue;
             }
-
             return res;
         }
 
 
 
-        private static int HzToPeriodInSamples(double hz, int sampleRate)
+        private static int hzToPeriodInSamples(double hz, int sampleRate)
         {
             return (int)(1 / (hz / (double)sampleRate));
         }
-        private static double PeriodInSamplesToHz(int period, int sampleRate)
+        private static double periodInSamplesToHz(int period, int sampleRate)
         {
             return 1 / (period / (double)sampleRate);
         }
-
-        /*
-         * The idea was that one could break a wave input into sine waves. This didn't work so well.
-        public static WaveAudio reconstruct(WaveAudio input)
-        {
-            if (input.getNumChannels() != 1) throw new Exception("Only mono supported.");
-            // turn it into sine waves ?!
-
-            WaveAudio reconstructed = new WaveAudio(input.getSampleRate(), 1);
-            reconstructed.LengthInSamples = input.LengthInSamples;
-
-            WaveAudio slice = new WaveAudio(input.getSampleRate(), 1);
-            const double SLICELEN = 0.094; //0.005;
-            slice.LengthInSeconds = SLICELEN;
-            int nSize = slice.LengthInSamples;
-            for (int i = 0; i < input.LengthInSamples-nSize; i += nSize)
-            {
-                // fill the slice with input.
-                Array.Copy(input.data[0], i, slice.data[0], 0, nSize);
-                // now find the pitch of the slice
-                //double[] pitches = amdf(slice, 30, 500, 1, 1);
-                double[] pitches = autocorrelate(slice, 200, 500, 1, 1);
-                // create a sine wave of that pitch, as long as the slice
-                WaveAudio sine = new Sine(pitches[0], 0.7).CreateWaveAudio(SLICELEN + 0.001);
-                // copy the sine to the output
-                Array.Copy(sine.data[0], 0, reconstructed.data[0], i, nSize);
-            }
-            return reconstructed;
-        }*/
-
     }
 }
